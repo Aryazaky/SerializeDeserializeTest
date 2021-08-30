5e7ec1c8e4dadccfaf4aab9296ad7e4a71f41f42
Aryazaky <aryazakyazif@gmail.com>
2021-08-31 00:18:28 +0700

    Tambah unit test

---
+++ SerializerProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SerializeDeserializeTest;
+
+namespace SerializerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ UnitTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = @"C:\Users\Keluarga\source\repos\SerializeDeserializeTest\SerializeDeserializeTest\bin\Debug\SerializeDeserializeTest.exe";
+            process.Start();
+            process.WaitForExit();
+            if(process.ExitCode == 0)
+            {
+                Console.WriteLine("SerializeDeserialize success");
+                if (File.Exists(@"D:\SerializedPlayerData.txt"))
+                {
+                    Console.WriteLine("Serialized file exists!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
